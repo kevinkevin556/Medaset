d4303ff467f6092e4eec685176560bdf1d271de2
Kevin Hong <kevinkevin556@yahoo.com.tw>
2023-12-27 18:16:18 +0800

    Fix bug: BackgroundifyClasses transformation returned torch.Tensor -> return MetaTensor without omitting meta-data

--- src/medaset/transforms.py
+++ src/medaset/transforms.py
@@ -1,11 +1,9 @@
 from typing import Dict, Hashable, Mapping, Optional
 
-import numpy as np
 import torch
 from monai.config import KeysCollection
-from monai.data.meta_obj import get_track_meta
+from monai.data.meta_tensor import MetaTensor
 from monai.transforms import MapTransform, Transform
-from monai.utils import convert_to_tensor
 
 __all__ = [
     "ApplyMaskMapping",
@@ -53,26 +51,34 @@ class BackgroundifyClasses(Transform):
     merging them into the background class.
     """
 
-    def __init__(self, classes: list, dim=0) -> None:
+    def __init__(self, channel_dim: int, classes: list) -> None:
+        self.channel_dim = channel_dim
         self.classes = torch.tensor(classes)
-        self.dim = dim
 
     def __call__(self, img) -> torch.Tensor:
-        ch = img.shape[self.dim]
+        n_channels = img.shape[self.channel_dim]
+
         # Create indexing tuple to retrieve background anc the merging class
-        # E.g. ix_bg = (0, slice(None), slice(None)) for the background of image  = img[0, :, :] when dim = 0
+        # E.g. ix_bg = (0, slice(None), slice(None)) for the background of image = img[0, :, :] when dim = 0
         #      ix_cls = (slice(None), self.classes, slice(None), slice(None))
         #      for the merging classes = img[:, classes, :, :] when dim = 1
         ix_bg = tuple([0 if (d == self.dim) else slice(None) for d in range(len(img.shape))])
         ix_cls = tuple([self.classes if (d == self.dim) else slice(None) for d in range(len(img.shape))])
         img_t = torch.Tensor(img)
-        # If ch == 1, the input tensor stores labels.
+
+        # If n_channels == 1, the input tensor stores labels.
         # Otherwise, the input tensor is a tensor of activations (probabilities) or results from one-hot encoding.
-        if ch == 1:
+        if n_channels == 1:
             img_t[torch.isin(img_t, self.classes)] = 0
         else:
             img_t[ix_bg] += torch.sum(img_t[ix_cls], dim=self.dim)
             img_t[ix_cls] = 0
+
+        # Collect meta-data from original MetaTensor, or return a plain MetaTensor
+        if isinstance(img, MetaTensor):
+            img_t = MetaTensor(img_t, meta=img.meta)
+        else:
+            img_t = MetaTensor(img_t)
         return img_t
 
 
@@ -84,11 +90,12 @@ class BackgroundifyClassesd(MapTransform):
     def __init__(
         self,
         keys: KeysCollection,
+        channel_dim: int,
         classes: list,
         allow_missing_keys: bool = False,
     ) -> None:
         MapTransform.__init__(self, keys, allow_missing_keys)
-        self.backgroundifier = BackgroundifyClasses(classes)
+        self.backgroundifier = BackgroundifyClasses(channel_dim, classes)
 
     def __call__(self, data: Mapping[Hashable, torch.Tensor]) -> Dict[Hashable, torch.Tensor]:
         d = dict(data)
